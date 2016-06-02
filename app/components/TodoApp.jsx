72222b15d9dba0c95700ad39d049cf0109f83973
Nicklas Winger <nwi@gecko.no>
2016-06-02 17:36:16 +0200

    Create AddTodo component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
   getInitialState: function() {
@@ -25,11 +26,18 @@ var TodoApp = React.createClass({
     };
   },
 
+  handleAddTodo: function(text) {
+    alert('New todo: ' + text);
+  },
+
   render: function() {
     var {todos} = this.state;
 
     return (
-      <TodoList todos={todos} />
+      <div>
+        <TodoList todos={todos} />
+        <AddTodo onAddTodo={this.handleAddTodo}/>
+      </div>
     );
   }
 });
--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var reactDOM = require('react-dom');
+var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jQuery');
