7931f60a5e8c19d8d4b0821bca48298fd5eb18a9
Nicklas Winger <winger.nicklas@gmail.com>
2016-06-11 17:38:45 +0200

    Update todos on firebase. Breaking tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -23,16 +23,13 @@ export var todosReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [...state, action.todo];
-    case 'TOGGLE_TODO':
+    case 'UPDATE_TODO':
       return state.map((todo) => {
         if (todo.id === action.id) {
-          var nextCompleted = !todo.completed;
-
           return {
             ...todo,
-            completed: nextCompleted,
-            completedAt: nextCompleted ? moment().unix() : undefined
-          };
+            ...action.updates
+          }
         } else {
           return todo;
         }
