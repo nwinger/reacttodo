b9ae3b61b44e95d420a9ec261d92b74ff30c0c69
Nicklas Winger <nwi@gecko.no>
2016-06-13 15:36:55 +0200

    Move firebase credentials to environment variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,10 +3,10 @@ import firebase from 'firebase';
 try {
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBQD3479_L5vQmR5O6amIbKCc6PH3G--Lo",
-    authDomain: "nwi-todo-app.firebaseapp.com",
-    databaseURL: "https://nwi-todo-app.firebaseio.com",
-    storageBucket: "nwi-todo-app.appspot.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
   };
   firebase.initializeApp(config);
 } catch (e) {
