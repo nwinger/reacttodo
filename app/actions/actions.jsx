ec02d886b7e2b3520932f1144bdada7a02069769
Nicklas Winger <nwi@gecko.no>
2016-06-16 09:27:28 +0200

    Remove log statements

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -108,18 +108,15 @@ export var logout = () => {
 
 export var startLogin = () => {
   return (dispatch, getState) => {
-    return firebase.auth().signInWithPopup(githubProvider).then((result) => {
-      console.log('Auth worked', result);
-    }, (error) => {
-      console.log('Unable to auth', error);
+    return firebase.auth().signInWithPopup(githubProvider)
+      .then((result) => {}, (error) => {
+        console.log('Unable to auth', error);
     });
   };
 };
 
 export var startLogout = () => {
   return (dispatch, getState) => {
-    return firebase.auth().signOut().then(() => {
-        console.log('Logged out!');
-    });
+    return firebase.auth().signOut().then(() => {});
   };
 };
