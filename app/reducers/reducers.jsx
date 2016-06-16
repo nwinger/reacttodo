6183c4c8106d059f08b17cd64e261c7ef234f6dd
Nicklas Winger <nwi@gecko.no>
2016-06-16 10:20:41 +0200

    Wipe todos on logout

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -42,6 +42,9 @@ export var todosReducer = (state = [], action) => {
         ...state,
         ...action.todos
       ];
+
+    case 'LOGOUT':
+      return [];
     default:
       return state;
   }
